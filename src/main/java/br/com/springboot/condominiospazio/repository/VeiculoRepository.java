6626180f320a5d9d11bb957ba59a7a97da1af374
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2025-03-16 21:34:18 -0300

    Criando classe Veiculo VeiculoService, VeiculoRepository e VeiculoController

---
+++ src/main/java/br/com/springboot/condominiospazio/repository/VeiculoRepository.java
@@ -0,0 +1,14 @@
+package br.com.springboot.condominiospazio.repository;
+
+import br.com.springboot.condominiospazio.model.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+
+    List<Veiculo> findByPlaca(String placa);
+
+}
